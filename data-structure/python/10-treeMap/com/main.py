80510f612233d10f9bf4dc48ae97df3d68505051
apple <1243684438@qq.com>
2019-06-21 10:46:24 +0800

    更新python版的TreeMap

---
+++ data-structure/python/10-treeMap/com/main.py
@@ -0,0 +1,31 @@
+from com.jqc.map.treeMap import TreeMap
+from com.jqc.map.map import Visitor
+
+
+def visit(key, value):
+	print(key, value)
+	# return True if key == 'c' else False
+	return False
+
+
+if __name__ == '__main__':
+	visitor = Visitor(visit)
+	cus_map = TreeMap()
+	cus_map.put("c", 2)
+	cus_map.put("a", 5)
+	cus_map.put("b", 6)
+	cus_map.put("a", 8)
+	cus_map.put("e", None)
+	cus_map.put("f", 8)
+	
+	# print(cus_map)
+	print('-----*****-----')
+	cus_map.traversal(visitor)
+	
+	print('-----*****-----')
+	print(cus_map.contains_value(None))
+	print(cus_map.get('a'))
+	
+	print('-----*****-----')
+	print(cus_map.remove('a'))
+	cus_map.traversal(visitor)
